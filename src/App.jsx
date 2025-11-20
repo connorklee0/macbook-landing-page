40cb42ce5e5b6a6e00c182a41bce53f89c470579
connorklee0 <connorklee0@gmail.com>
2025-11-20 14:34:07 -0500

    implement performance section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import ProductViewer from "./components/ProductViewer";
+import Performance from "./components/Performance";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Showcase from "./components/Showcase";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,6 +19,10 @@ const App = () => {
       <Hero />
       <ProductViewer />
       <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   );
 };
